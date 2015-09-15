ca1161bc1930b66dfd660f07a00b4c8499986aea
Kc kamakazi <kc@kc-Lenovo-G500.(none)>
2015-09-15 08:20:42 +0530

    second

--- tutorial2/settings.py
+++ tutorial2/settings.py
@@ -99,5 +99,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfile")
+STATIC_URL = "/static/"
 
-STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+   ('assets', os.path.join(BASE_DIR, "static")),
+)
+
+
